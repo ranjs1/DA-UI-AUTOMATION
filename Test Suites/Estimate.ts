2621dd435ced356a3f41e837d44682cb482c43c4
ranjs1 <sumit.ranjan@in.pega.com>
2018-08-28 11:19:58 +0530

    Ranjs1

--- Test Suites/Estimate.ts
+++ Test Suites/Estimate.ts
@@ -4,7 +4,7 @@
    <name>Estimate</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-08-27T15:41:14</lastRun>
+   <lastRun>2018-08-28T10:16:08</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
